737ca691ed3f1c498b7c3fc599f09dd805feddb3
Sigurd <sigurdta@gmail.com>
2024-02-17 16:46:28 +0100

    no message

--- Kulhydrat+/Kulhydrat_App.swift
+++ Kulhydrat+/Kulhydrat_App.swift
@@ -4,14 +4,19 @@
 //
 //  Created by Sigurd Andersson on 10/11/2023.
 //
-
+/*
 import SwiftUI
+import Firebase
 
 @main
 struct Kulhydrat_App: App {
+    init(){
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+*/
